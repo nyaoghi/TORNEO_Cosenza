389ba0d07b22519de8de5ba831e0a7eb78b296dd
Cosenza Olga <olgacosenza06@gmail.com>
2024-11-28 09:14:24 +0100

    structure update

--- src/torneo_cosenza/TORNEO_Cosenza.java
+++ src/torneo_cosenza/TORNEO_Cosenza.java
@@ -14,7 +14,7 @@ public class TORNEO_Cosenza {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("prova sincronizzazione");
+         
         
         // TODO code application logic here
     }
