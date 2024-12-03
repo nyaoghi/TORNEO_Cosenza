c0a1f03068a1755ed8e313eca938f25dc22b2b18
Cosenza Olga <olgacosenza06@gmail.com>
2024-12-03 10:37:34 +0100

    modifica sul ramo squadra

--- src/torneo_cosenza/TORNEO_Cosenza.java
+++ src/torneo_cosenza/TORNEO_Cosenza.java
@@ -14,6 +14,7 @@ public class TORNEO_Cosenza {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+         System.out.println("Modifica 2");
          
         
         // TODO code application logic here
